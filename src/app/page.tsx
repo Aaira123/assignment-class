b0b22da9a430bda3d69a7e86c1a70a600147b244
sana arshad <160604463+Aaira123@users.noreply.github.com>
2024-10-19 05:30:46 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  return ( 
+    <div>
+    <ul className="flex gap-10 bg-blue-300">
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li><Link  href="/about">About</Link></li>
+      <li><Link href="/contact-us">contact us</Link></li>
+    </ul>
+    <img src="https://media.licdn.com/dms/image/D4E12AQEIqlkU8NSvJg/article-cover_image-shrink_600_2000/0/1692105491054?e=2147483647&v=beta&t=5a8EH7I7NCdUmV13-itNvChm4W_yJOQxx4iFqbaEilE" alt="sorry" width="2000"/>
+    </div> )
+}
